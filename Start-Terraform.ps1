e2f5123378dcac8bdf1574c0b700580f820e1841
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:43:21 +0530

    Create main.tf, variable.tf, and output.tf if missing
    Add script to create Terraform files if they don't exist.

--- Start-Terraform.ps1
+++ Start-Terraform.ps1
@@ -1,1 +1,9 @@
+$CurLoc = (Get-Location).Path
+$CurLoc
 
+foreach ( $fn in ('main.tf', 'variable.tf', 'output.tf')) {
+    $tPath = $CurLoc + '\' + $fn
+    if ( -not (Test-path -Path $tPath)) {
+        New-Item -Path $CurLoc -Name $fn -ItemType File
+    }
+}
